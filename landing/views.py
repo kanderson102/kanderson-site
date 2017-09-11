3e7033b6f63baa20b8a37bd1af9b5d08d21c93d0
kanderson102 <kvanderson102@gmail.com>
2017-09-11 13:57:37 -0500

    added basic contact page, small changes

--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"landing/index.html", {})
+
+def contact(request):
+    return render(request,"landing/contact.html", {})
