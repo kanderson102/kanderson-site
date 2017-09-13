b08222146d3cc939f64e2706f0f31ac8874dbc95
Kyle <kvanderson102@gmail.com>
2017-09-13 12:11:30 -0500

    updated index

--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"landing/index.html", {})
-
-def contact(request):
-    return render(request,"landing/contact.html", {})
