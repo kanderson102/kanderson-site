e1cfd9cecdb6cc53072806209f04f76d51f9dc2f
Kyle <kvanderson102@gmail.com>
2017-10-12 12:43:40 -0500

    increased Models slugfield max_length

--- posts/models.py
+++ posts/models.py
@@ -17,7 +17,7 @@ def upload_location(instance, filename):
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, max_length=120)
     image = models.ImageField(upload_to=upload_location,
             null=True,
             blank=True,
